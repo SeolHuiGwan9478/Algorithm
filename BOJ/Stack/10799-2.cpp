4604d9e2e9971ade47e066f531a7181af58031c3
SeolHuiGwan9478 <shg5977@naver.com>
2023-01-18 01:45:28 +0900

    [BUG] 10799-2 주석 추가

--- BOJ/Stack/10799-2.cpp
+++ BOJ/Stack/10799-2.cpp
@@ -12,13 +12,13 @@ int main(){
     for(int i = 0;i < ps.length();i++){
         if(ps[i] == '(') s.push('(');
         else{
-            if(ps[i-1] == '('){
-                s.pop();
-                ans += s.size();
+            if(ps[i-1] == '('){ //레이저일 경우
+                s.pop(); //앞에서 막대라고 착각하고 stack에 s[i]를 넣었으므로 pop
+                ans += s.size(); //막대의 개수만큼 ans에 추가
             }
-            else{
-                s.pop();
-                ans++;
+            else{ //막대의 끝일 경우
+                s.pop(); //막대의 개수를 1 감소
+                ans++; //막대 1개가 절단된 것과 동일한 상황이므로 ans에 1 추가
             }
         }
     }
